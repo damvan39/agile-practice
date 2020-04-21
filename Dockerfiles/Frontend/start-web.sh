23e4b91c7927d54168e24f3a6eddf46345648111
Damian <damian.vdm52@gmail.com>
2020-04-22 11:28:25 +1200

    version3

--- Dockerfiles/Frontend/start-web.sh
+++ Dockerfiles/Frontend/start-web.sh
@@ -1,7 +1,4 @@
 echo "called sucsessfully"
-cp monit-frontend.conf /etc/monitrc
-chmod 700 /etc/monitrc
-mkdir /var/run/monit/
 if [ $CI ] ; then
     echo "***************************TEST MODE***************************"
     monit start all
@@ -16,6 +13,26 @@ if [ $CI ] ; then
     fi 
     exit 0
 fi
-echo "starting" & monit start all
-monit -I
-exit 0
+echo $nfirst
+if [ $NFIRST ] ; then
+    echo "Detected not first run"
+    tail -f /dev/null
+
+    else 
+    echo "first run"
+    cp monit-frontend.conf /etc/monitrc
+    chmod 700 /etc/monitrc
+    mkdir /var/run/monit/
+    echo "starting" 
+    monit start all
+    fi
+NFIRST="True"
+export NFIRST
+
+while true
+do
+    echo "entered loop"
+    monit -I
+    monit stop all
+done
+
