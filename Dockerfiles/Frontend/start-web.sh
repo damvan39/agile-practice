d2c7dd80039bd59ef5c8d27ba31389dbf9318b94
Damian Van Der Merwe <63829786+damvan39@users.noreply.github.com>
2020-04-21 16:05:09 +1200

    Update start-web.sh

--- Dockerfiles/Frontend/start-web.sh
+++ Dockerfiles/Frontend/start-web.sh
@@ -5,8 +5,16 @@ mkdir /var/run/monit/
 echo $CI
 if [ $CI ] ; then
     echo "test mode"
-    monit -t
+    monit
     sleep 3
+    wget localhost:80
+    if [ &? -eq 0 ]
+    then
+        echo "SUCCESS"
+    else
+        echo "unabe to reach port 80 FALILED"
+        exit 1
+    fi    
     exit 0
 fi
 monit -I
