d92535e1022ddd3be472b7145f97b0cf345e7ae1
Damian Van Der Merwe <63829786+damvan39@users.noreply.github.com>
2020-04-22 10:32:26 +1200

    Delete update-repo.sh

--- misc/update-repo.sh
+++
@@ -1,6 +0,0 @@
-cat > /etc/apk/repositories << EOF
-http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/main
-http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/community
-EOF
-
-apk update
