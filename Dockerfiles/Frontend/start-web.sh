b3a15c67a70b242cffccca2a07809d2910a990fd
Damian Van Der Merwe <63829786+damvan39@users.noreply.github.com>
2020-04-21 16:55:43 +1200

    Update start-web.sh

--- Dockerfiles/Frontend/start-web.sh
+++ Dockerfiles/Frontend/start-web.sh
@@ -7,12 +7,12 @@ if [ $CI ] ; then
     echo "test mode"
     monit start all
     sleep 3
-    wgt localhost:80
+    wget localhost:80
     if [ $? -eq 0 ]
     then
         echo "SUCCESS"
     else
-        echo "unabe to reach port 80 FALILED"
+        echo "unable to reach port 80 FALILED"
         exit 1
     fi    
     exit 0
